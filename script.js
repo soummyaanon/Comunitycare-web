9e9c20a0a1a618a9f9fb141792647d0818a5f5a2
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2023-12-19 18:32:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text',{
+    strings:['Join Us','  We care for the Nature','We are EnviroGuardians'],
+typeSpeed: 150,
+backSpeed: 150,
+backDelay: 150,
+loop: true
+});
