5169e70b32033959d395d004baa04697c7ff63c6
Soumyaranjan panda <91568463+soummyaanon@users.noreply.github.com>
2023-12-20 15:31:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,46 @@ backSpeed: 150,
 backDelay: 150,
 loop: true
 });
+
+ocument.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contactForm');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const fname = document.getElementById('fname').value.trim();
+        const lname = document.getElementById('lname').value.trim();
+        const email = document.getElementById('email').value.trim();
+        const subject = document.getElementById('subject').value.trim();
+        const rating = document.getElementById('rating').value;
+
+        // Check if any of the fields, including rating, is empty
+        if (!fname || !lname || !email || !subject || !rating) {
+            alert('Please fill in all fields, including the rating');
+            return;
+        }
+        
+        // Assuming submission was successful
+        // You can customize the success message as needed
+        alert('Submission successful! Thank you.');
+        
+        // Additional code for any other actions you want to perform after successful submission
+        // For example, you might want to reset the form or redirect the user to another page.
+        // You can add that code here.
+        
+
+
+        // Add your code to send the form data, including the rating, to Firebase or another backend.
+        // For demonstration purposes, we'll just log the data to the console.
+        console.log({
+            fname,
+            lname,
+            email,
+            subject,
+            rating
+        });
+
+        // You can continue with your existing form validation logic here.
+
+    });
+});
